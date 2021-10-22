3d7bd8f216b48c91b01561f4f7305982cc1701c8
pchevieux <philippe.chevieux@gmail.com>
2021-10-22 16:54:49 +0200

    Add buttons to switch between list and mosaic display
    Still in dev

--- src/pages/PlaylistList/index.jsx
+++ src/pages/PlaylistList/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useContext, useState } from 'react'
 import { getYoutubePlaylists } from '../../utils/api'
 import { GoogleAccountDataContext } from '../../utils/context/index'
 
-import { Breadcrumbs, Typography } from '@mui/material'
+import { Breadcrumbs, Typography, AppBar, Toolbar, IconButton } from '@mui/material'
 
 import './styles.css'
 
@@ -10,10 +10,14 @@ import MosaicMode from '../../components/Playlist/MosaicMode'
 import ListMode from '../../components/Playlist/ListMode'
 import HomeIcon from '@mui/icons-material/Home'
 
+import ViewModuleIcon from '@mui/icons-material/ViewModule'
+import ListIcon from '@mui/icons-material/List'
+
 function PlaylistList() {
     // Get googleAccountData context
     const { googleAccountData } = useContext(GoogleAccountDataContext)
     const [playlistsListData, setPlaylistsListData] = useState(null)
+    const [playlistActiveMode, setPlaylistActiveMode] = useState('mosaic')
 
     useEffect(() => {
         console.log('playlist list : ', googleAccountData)
@@ -30,20 +34,51 @@ function PlaylistList() {
 
     return (
         <div>
-            <div>
+            {/* <div>
                 <Breadcrumbs aria-label="breadcrumb">
-                    {/* <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/">
+                    <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/">
                         <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
                         Mes Playlists
-                    </Link> */}
+                    </Link>
                     <Typography sx={{ display: 'flex', alignItems: 'center' }} color="text.primary">
                         <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
                         Mes playlists
                     </Typography>
                 </Breadcrumbs>
-            </div>
+            </div> */}
 
-            {!playlistsListData ? <div>Rien</div> : <ListMode playlistsListData={playlistsListData} />}
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick=""
+                        color="inherit"
+                    >
+                        <ViewModuleIcon />
+                    </IconButton>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick=""
+                        color="inherit"
+                    >
+                        <ListIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+
+            {!playlistsListData && <div>Rien</div>}
+
+            {playlistsListData && playlistActiveMode === 'mosaic' && (
+                <MosaicMode playlistsListData={playlistsListData} />
+            )}
+
+            {playlistsListData && playlistActiveMode === 'list' && <ListMode playlistsListData={playlistsListData} />}
         </div>
     )
 }
