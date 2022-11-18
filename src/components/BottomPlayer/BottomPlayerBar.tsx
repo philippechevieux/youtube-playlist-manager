62f0d84acea5b16d6e0cb4dc5b4249100f8ec4c2
Philippe Chevieux <philippe.chevieux@gmail.com>
2022-11-18 21:37:13 +0100

    WIP: Improve volume controls display

--- src/components/BottomPlayer/BottomPlayerBar.tsx
+++ src/components/BottomPlayer/BottomPlayerBar.tsx
@@ -1,8 +1,9 @@
 import {PlayArrowOutlined, SkipNextOutlined, VolumeUpOutlined} from '@material-ui/icons';
 import {PauseOutlined, SkipPreviousOutlined, VolumeOffOutlined} from '@mui/icons-material';
-import {AppBar, IconButton, Slider, Toolbar} from '@mui/material';
+import {AppBar, Box, Grid, IconButton, Slider, Stack, Toolbar} from '@mui/material';
 import {useState} from 'react';
 import {YouTubeEvent} from 'react-youtube';
+import './styles.css';
 
 function BottomPlayerBar({
     playlistId,
@@ -23,6 +24,8 @@ function BottomPlayerBar({
     isMuted: boolean;
     setIsMuted: Function;
 }) {
+    const [shouldSliderBeDisplayed, setShouldSliderBeDisplayed] = useState(false);
+
     const onPlayPauseClick = () => {
         setIsVideoPaused(!isVideoPaused);
         isVideoPaused ? player.playVideo() : player.pauseVideo();
@@ -49,21 +52,45 @@ function BottomPlayerBar({
     return playlistId !== undefined ? (
         <AppBar position="fixed" sx={{bottom: 0}}>
             <Toolbar>
-                <IconButton color="inherit" onClick={onPreviousClick}>
-                    <SkipPreviousOutlined />
-                </IconButton>
-                <IconButton color="inherit" onClick={onPlayPauseClick}>
-                    {isVideoPaused ? <PlayArrowOutlined /> : <PauseOutlined />}
-                </IconButton>
-                <IconButton color="inherit" onClick={onNextClick}>
-                    <SkipNextOutlined />
-                </IconButton>
-                <div className="volume-wrapper">
-                    <Slider size="small" min={0} max={100} value={volume} onChange={onVolumeChange} />
-                    <IconButton color="inherit" onClick={onMuteUnMuteClick}>
-                        {isMuted ? <VolumeOffOutlined /> : <VolumeUpOutlined />}
-                    </IconButton>
-                </div>
+                <Grid container spacing={3} alignItems="center" justifyContent="space-between">
+                    <Grid item xs={3}>
+                        <div>test</div>
+                    </Grid>
+                    <Grid item xs={6} textAlign={'center'}>
+                        <IconButton color="inherit" onClick={onPreviousClick}>
+                            <SkipPreviousOutlined />
+                        </IconButton>
+                        <IconButton color="inherit" onClick={onPlayPauseClick}>
+                            {isVideoPaused ? <PlayArrowOutlined /> : <PauseOutlined />}
+                        </IconButton>
+                        <IconButton color="inherit" onClick={onNextClick}>
+                            <SkipNextOutlined />
+                        </IconButton>
+                    </Grid>
+                    <Grid item xs={3} textAlign={'end'} width={'100%'}>
+                        <Stack direction="row" justifyContent="flex-end" alignItems="center">
+                            <Slider
+                                className={`volume-slider ${!shouldSliderBeDisplayed ? 'hidden' : ''}`}
+                                size="small"
+                                min={0}
+                                max={100}
+                                value={volume}
+                                onChange={onVolumeChange}
+                                onMouseEnter={() => setShouldSliderBeDisplayed(true)}
+                                onMouseLeave={() => setShouldSliderBeDisplayed(false)}
+                            />
+                            <IconButton
+                                className="volume-icon"
+                                color="inherit"
+                                onClick={onMuteUnMuteClick}
+                                onMouseEnter={() => setShouldSliderBeDisplayed(true)}
+                                onMouseLeave={() => setShouldSliderBeDisplayed(false)}
+                            >
+                                {isMuted ? <VolumeOffOutlined /> : <VolumeUpOutlined />}
+                            </IconButton>
+                        </Stack>
+                    </Grid>
+                </Grid>
             </Toolbar>
         </AppBar>
     ) : (
