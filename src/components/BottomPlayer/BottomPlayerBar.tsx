4a2ff986428a758f84b3b8799b79276f5a81740a
Philippe Chevieux <philippe.chevieux@gmail.com>
2022-11-20 18:22:19 +0100

    WIP: Adding a progress bar

--- src/components/BottomPlayer/BottomPlayerBar.tsx
+++ src/components/BottomPlayer/BottomPlayerBar.tsx
@@ -6,6 +6,7 @@ import {
     Box,
     Grid,
     IconButton,
+    LinearProgress,
     ListItem,
     ListItemAvatar,
     ListItemText,
@@ -14,7 +15,7 @@ import {
     Toolbar,
     Typography
 } from '@mui/material';
-import {useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {YouTubeEvent} from 'react-youtube';
 import {ItemInterface} from '../../utils/arms/playlistContents/state';
 import {getThumbnailsFromItem} from '../../utils/Functions';
@@ -42,6 +43,32 @@ function BottomPlayerBar({
     setIsMuted: Function;
 }) {
     const [shouldSliderBeDisplayed, setShouldSliderBeDisplayed] = useState(false);
+    const [progress, setProgress] = useState(0);
+
+    const progressRef = useRef(() => {});
+    useEffect(() => {
+        progressRef.current = () => {
+            if (progress > 100) {
+                setProgress(0);
+            } else {
+                const currentTime = player?.playerInfo.currentTime !== undefined ? player.playerInfo.currentTime : 0;
+                const duration = player?.playerInfo.duration !== undefined ? player.playerInfo.duration : 100;
+
+                const newProgress = (currentTime / duration) * 100;
+                setProgress(newProgress);
+            }
+        };
+    });
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            progressRef.current();
+        }, 500);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
 
     const onPlayPauseClick = () => {
         setIsVideoPaused(!isVideoPaused);
@@ -57,7 +84,7 @@ function BottomPlayerBar({
     };
 
     const onVolumeChange = (e: Event, value: number | number[]) => {
-        setVolume(value); //TODO: check later is this set is usefull (maybe to sync iframe volume slider)
+        setVolume(value);
         player.setVolume(volume);
     };
 
@@ -67,68 +94,73 @@ function BottomPlayerBar({
     };
 
     return playlistId !== undefined ? (
-        <AppBar className="bottom-player-bar" position="fixed" sx={{bottom: 0}}>
-            <Toolbar>
-                <Grid container spacing={3} alignItems="center" justifyContent="space-between">
-                    <Grid item xs={4}>
-                        {contentItem !== undefined && (
-                            <Stack direction="row" justifyContent="flex-start" alignItems="center">
-                                <Avatar
-                                    className="avatar-thumbnail"
-                                    sx={{width: 94, height: 64}}
-                                    alt={contentItem.snippet.title}
-                                    src={getThumbnailsFromItem(contentItem)}
-                                    variant="square"
-                                />
-                                <div className="info">
-                                    <Typography className="title" variant="h6" color="text.primary">
-                                        {contentItem.snippet.title}
-                                    </Typography>
+        <>
+            <AppBar className="bottom-player-bar" position="fixed" sx={{bottom: 0}}>
+                <Box sx={{width: '100%'}}>
+                    <LinearProgress className="progress-bar" variant="determinate" value={progress} color="secondary" />
+                </Box>
+                <Toolbar>
+                    <Grid container spacing={3} alignItems="center" justifyContent="space-between">
+                        <Grid item xs={4}>
+                            {contentItem !== undefined && (
+                                <Stack direction="row" justifyContent="flex-start" alignItems="center">
+                                    <Avatar
+                                        className="avatar-thumbnail"
+                                        sx={{width: 94, height: 64}}
+                                        alt={contentItem.snippet.title}
+                                        src={getThumbnailsFromItem(contentItem)}
+                                        variant="square"
+                                    />
+                                    <div className="info">
+                                        <Typography className="title" variant="h6" color="text.primary">
+                                            {contentItem.snippet.title}
+                                        </Typography>
 
-                                    <Typography className="author" variant="body2" color="text.secondary">
-                                        {contentItem.snippet.videoOwnerChannelTitle}
-                                    </Typography>
-                                </div>
-                            </Stack>
-                        )}
-                    </Grid>
-                    <Grid item xs={4} textAlign={'center'}>
-                        <IconButton color="inherit" onClick={onPreviousClick}>
-                            <SkipPreviousOutlined />
-                        </IconButton>
-                        <IconButton color="inherit" onClick={onPlayPauseClick}>
-                            {isVideoPaused ? <PlayArrowOutlined /> : <PauseOutlined />}
-                        </IconButton>
-                        <IconButton color="inherit" onClick={onNextClick}>
-                            <SkipNextOutlined />
-                        </IconButton>
-                    </Grid>
-                    <Grid item xs={4} textAlign={'end'} width={'100%'}>
-                        <Stack direction="row" justifyContent="flex-end" alignItems="center">
-                            <Slider
-                                className={`volume-slider ${!shouldSliderBeDisplayed ? 'hidden' : ''}`}
-                                size="small"
-                                min={0}
-                                max={100}
-                                value={volume}
-                                onChange={onVolumeChange}
-                                onMouseEnter={() => setShouldSliderBeDisplayed(true)}
-                                onMouseLeave={() => setShouldSliderBeDisplayed(false)}
-                            />
-                            <IconButton
-                                className="volume-icon"
-                                color="inherit"
-                                onClick={onMuteUnMuteClick}
-                                onMouseEnter={() => setShouldSliderBeDisplayed(true)}
-                                onMouseLeave={() => setShouldSliderBeDisplayed(false)}
-                            >
-                                {isMuted ? <VolumeOffOutlined /> : <VolumeUpOutlined />}
+                                        <Typography className="author" variant="body2" color="text.secondary">
+                                            {contentItem.snippet.videoOwnerChannelTitle}
+                                        </Typography>
+                                    </div>
+                                </Stack>
+                            )}
+                        </Grid>
+                        <Grid item xs={4} textAlign={'center'}>
+                            <IconButton color="inherit" onClick={onPreviousClick}>
+                                <SkipPreviousOutlined />
                             </IconButton>
-                        </Stack>
+                            <IconButton color="inherit" onClick={onPlayPauseClick}>
+                                {isVideoPaused ? <PlayArrowOutlined /> : <PauseOutlined />}
+                            </IconButton>
+                            <IconButton color="inherit" onClick={onNextClick}>
+                                <SkipNextOutlined />
+                            </IconButton>
+                        </Grid>
+                        <Grid item xs={4} textAlign={'end'} width={'100%'}>
+                            <Stack direction="row" justifyContent="flex-end" alignItems="center">
+                                <Slider
+                                    className={`volume-slider ${!shouldSliderBeDisplayed ? 'hidden' : ''}`}
+                                    size="small"
+                                    min={0}
+                                    max={100}
+                                    value={volume}
+                                    onChange={onVolumeChange}
+                                    onMouseEnter={() => setShouldSliderBeDisplayed(true)}
+                                    onMouseLeave={() => setShouldSliderBeDisplayed(false)}
+                                />
+                                <IconButton
+                                    className="volume-icon"
+                                    color="inherit"
+                                    onClick={onMuteUnMuteClick}
+                                    onMouseEnter={() => setShouldSliderBeDisplayed(true)}
+                                    onMouseLeave={() => setShouldSliderBeDisplayed(false)}
+                                >
+                                    {isMuted ? <VolumeOffOutlined /> : <VolumeUpOutlined />}
+                                </IconButton>
+                            </Stack>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Toolbar>
-        </AppBar>
+                </Toolbar>
+            </AppBar>
+        </>
     ) : (
         <></>
     );
