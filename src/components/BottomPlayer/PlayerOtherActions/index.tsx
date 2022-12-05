38dba8924730e69ede2c3d47de3db8f5e9704e4d
Philippe Chevieux <philippe.chevieux@gmail.com>
2022-12-05 22:46:33 +0100

    WIP : Add shuffle button

--- src/components/BottomPlayer/PlayerOtherActions/index.tsx
+++ src/components/BottomPlayer/PlayerOtherActions/index.tsx
@@ -1,4 +1,10 @@
-import {ArrowDropDownOutlined, ShuffleOutlined, VolumeOffOutlined, VolumeUpOutlined} from '@mui/icons-material';
+import {
+    ArrowDropDownOutlined,
+    ShuffleOnOutlined,
+    ShuffleOutlined,
+    VolumeOffOutlined,
+    VolumeUpOutlined
+} from '@mui/icons-material';
 import {IconButton, Slider, Stack} from '@mui/material';
 import {useState} from 'react';
 import {YouTubeEvent} from 'react-youtube';
@@ -15,6 +21,7 @@ function PlayerOtherActions({
     const [volume, setVolume] = useState<number | number[]>(100);
     const [shouldSliderBeDisplayed, setShouldSliderBeDisplayed] = useState(false);
     const [isMuted, setIsMuted] = useState(false);
+    const [isShuffle, setIsShuffle] = useState(false);
 
     const onVolumeChange = (event: Event, value: number | number[]) => {
         if (typeof value === 'number') {
@@ -26,6 +33,13 @@ function PlayerOtherActions({
     const onMuteUnMuteClick = () => {
         setIsMuted(!isMuted);
         isMuted ? player.unMute() : player.mute();
+    };
+
+    const onShuffleClick = () => {
+        const newIsShuffle = !isShuffle;
+
+        setIsShuffle(newIsShuffle);
+        player.setShuffle(newIsShuffle);
     };
 
     return (
@@ -47,8 +61,8 @@ function PlayerOtherActions({
             >
                 {isMuted ? <VolumeOffOutlined /> : <VolumeUpOutlined />}
             </IconButton>
-            <IconButton color="inherit" onClick={() => {}}>
-                <ShuffleOutlined />
+            <IconButton color="inherit" onClick={onShuffleClick}>
+                {isShuffle ? <ShuffleOnOutlined /> : <ShuffleOutlined />}
             </IconButton>
             <IconButton color="inherit" onClick={() => setIsIFrameToggled(!isIFrameToggled)}>
                 <ArrowDropDownOutlined className="drop-down-up-icon" fontSize="large" />
