55382caea3ae4fc49d2c72123e0d4175c8ceb309
Philippe Chevieux <philippe.chevieux@gmail.com>
2022-12-12 22:54:23 +0100

    WIP: Add footer content

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -1,4 +1,4 @@
-import {Divider} from '@mui/material';
+import {Box, Divider, Grid, Link} from '@mui/material';
 import {useTranslation} from 'react-i18next';
 import './styles.css';
 
@@ -8,6 +8,43 @@ function Footer() {
     return (
         <div className="footer">
             <Divider />
+            <Box className="footer-content">
+                <Grid container direction="row" spacing={4} justifyContent="center">
+                    <Grid item>
+                        <Link
+                            href="https://github.com/philippechevieux"
+                            target="_blank"
+                            variant="body2"
+                            color="text.secondary"
+                            underline="hover"
+                        >
+                            {t('made with and by')}
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link
+                            href="https://github.com/philippechevieux/youtube-playlist-manager/issues"
+                            target="_blank"
+                            variant="body2"
+                            color="text.secondary"
+                            underline="hover"
+                        >
+                            {t('suggestion')}
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link
+                            href="https://github.com/philippechevieux/youtube-playlist-manager/issues"
+                            target="_blank"
+                            variant="body2"
+                            color="text.secondary"
+                            underline="hover"
+                        >
+                            {t('report bug')}
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
     );
 }
